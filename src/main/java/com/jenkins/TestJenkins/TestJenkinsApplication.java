9eeb54c18d2f25ee311e99853d3b5008294bda16
udaydhope <udays@bextra.io>
2021-05-23 18:20:25 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/TestJenkins/TestJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.jenkins.TestJenkins;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class TestJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(TestJenkinsApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application Startred");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application Executed........");
+		SpringApplication.run(TestJenkinsApplication.class, args);
+	}
+
+}
